8101f2b91365fcacac6b4eba6e2218d1fe2a5e52
cwq940061186 <940061186@qq.com>
2018-02-14 02:54:43 +0800

    update

---
+++ js/baiduhm.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?77fc6949cf69f0bbd886cfa8dd21bea4";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
