a6dd3261c4cd2f56ebb0c36664d60ac38a824a06
cwq940061186 <940061186@qq.com>
2018-02-24 01:33:21 +0800

    回滚到博客模式
    回滚到博客模式

--- js/baiduhm.js
+++ js/baiduhm.js
@@ -2,7 +2,7 @@ var _hmt = _hmt || [];
 
 (function () {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?77fc6949cf69f0bbd886cfa8dd21bea4";
+    hm.src = "https://hm.baidu.com/hm.js?8e17d83d66244c4ddef1b0474277f7c8";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
